3488e763d3356b7cc385cb3ec780746e7f80f0be
Anthea Merton <anthea.merton@gmail.com>
2019-03-20 16:45:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,8 +68,7 @@ end
 def runner
   # code runner here
   welcome 
-  card total=i
-  initial_round
+  card_total=initial_round
   hit?(card_total)
   display_card_total(card_total) 
   until display_card_total>21 
